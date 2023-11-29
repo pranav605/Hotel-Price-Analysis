4360f99b5922486c84301e60e08107bbf4844222
akshar2223 <88324261+akshar2223@users.noreply.github.com>
2023-11-29 14:29:35 -0500

    Update Searchfrequency.java

--- src/Searchfrequency.java
+++ src/Searchfrequency.java
@@ -3,78 +3,70 @@ import java.io.*;
 import java.util.*;
 
 public class Searchfrequency {
-
-    public static void main(String[] args) {
-        try {
-            updateSearches();
-            Map<String, Integer> searchFrequency = indexMultipleFiles();
-            displaySearchFrequency(searchFrequency);
-        } catch (IOException e) {
-            e.printStackTrace();
+        public static void updateSearches() throws IOException {
+        // Read data from parameters.json
+        String parametersFilePath = "parameters.json"; // Replace with your parameters file path
+        JsonObject parameters;
+        try (BufferedReader reader = new BufferedReader(new FileReader(parametersFilePath))) {
+            parameters = JsonParser.parseReader(reader).getAsJsonObject();
         }
-    }
-
-    public static void updateSearches() throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a destination:");
-        String destination = scanner.nextLine();
-
-        // Read existing data from the JSON file
-        String filePath = "userSearches.json"; // Replace with your JSON file containing user search history
+        // Extract city, state/country information and convert to lowercase
+        String enteredCity = parameters.get("enteredCity").getAsString().toLowerCase();
+        String enteredState = parameters.has("enteredState") ? parameters.get("enteredState").getAsString().toLowerCase() : "";
+        String enteredCountry = parameters.has("enteredCountry") ? parameters.get("enteredCountry").getAsString().toLowerCase() : "";
+        // Create or read existing data from the userSearches.json file
+        String userSearchesFilePath = "userSearches.json"; // Replace with your JSON file containing user search history
         JsonArray jsonArray;
-        if (new File(filePath).exists()) {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            jsonArray = JsonParser.parseReader(reader).getAsJsonArray();
-            reader.close();
+        if (new File(userSearchesFilePath).exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(userSearchesFilePath))) {
+                jsonArray = JsonParser.parseReader(reader).getAsJsonArray();
+            }
         } else {
             jsonArray = new JsonArray();
         }
-
-        // Append the new destination to the JSON array
+        // Create a new search object with the extracted lowercase information
         JsonObject newSearch = new JsonObject();
-        newSearch.addProperty("search_term", destination);
+        newSearch.addProperty("city", enteredCity);
+        newSearch.addProperty("state", enteredState);
+        newSearch.addProperty("country", enteredCountry);
+        // Add the new search object to the JSON array
         jsonArray.add(newSearch);
-
-        // Write the updated data back to the JSON file
-        FileWriter writer = new FileWriter(filePath);
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        writer.write(gson.toJson(jsonArray));
-        writer.close();
+        // Write the updated data back to the userSearches.json file
+        try (FileWriter writer = new FileWriter(userSearchesFilePath)) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            writer.write(gson.toJson(jsonArray));
+        }
     }
-
     public static Map<String, Integer> indexMultipleFiles() throws IOException {
         String filePath = "userSearches.json"; // Replace with your JSON file containing user search history
-
         Map<String, Integer> searchFrequency = new HashMap<>();
-
         if (new File(filePath).exists()) {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            JsonArray jsonArray = JsonParser.parseReader(reader).getAsJsonArray();
-            processJsonArray(jsonArray, searchFrequency);
-            reader.close();
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                JsonArray jsonArray = JsonParser.parseReader(reader).getAsJsonArray();
+                processJsonArray(jsonArray, searchFrequency);
+            }
         }
-
         return searchFrequency;
     }
-
     public static void processJsonArray(JsonArray jsonArray, Map<String, Integer> searchFrequency) {
         for (JsonElement element : jsonArray) {
             JsonObject jsonObject = element.getAsJsonObject();
 
-            if (jsonObject.has("search_term")) {
-                String searchTerm = jsonObject.get("search_term").getAsString();
+            if (jsonObject.has("city") && jsonObject.has("state") && jsonObject.has("country")) {
+                String searchTerm = jsonObject.get("city").getAsString() + ", " +
+                        jsonObject.get("state").getAsString() + ", " +
+                        jsonObject.get("country").getAsString();
                 int frequency = searchFrequency.getOrDefault(searchTerm, 0);
                 searchFrequency.put(searchTerm, frequency + 1);
             }
         }
     }
-
     public static void displaySearchFrequency(Map<String, Integer> searchFrequency) {
         System.out.println("***********************************");
-        System.out.println("Search Frequency:");
-
+        System.out.println("***********************************");
+        System.out.println("Most Searched Destinations:");
         searchFrequency.entrySet().forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue() + " times"));
-
+        System.out.println("***********************************");
         System.out.println("***********************************");
     }
 }
