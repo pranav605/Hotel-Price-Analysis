1be4a1d2370b6e973e40657ac7baf28ebddaf50d
koratvaibhav123@gmail.com <koratvaibhav123@gmail.com>
2023-11-29 22:09:21 -0500

    changes in frequency count

--- src/FrequencyCount.java
+++ src/FrequencyCount.java
@@ -6,36 +6,43 @@ import com.google.gson.reflect.TypeToken;
 
 public class FrequencyCount {
 
-	// Inner class for handle structure of Hotel data.
+    // Inner class to handle the structure of Hotel data.
     public static class HotelData {
         String name, location, reviewMessage;
         List<Room> rooms;
         List<String> amenities;
     }
-   //Inner class for handle structure of Room data within each Hotel.
+
+    // Inner class to handle the structure of Room data within each Hotel.
     public static class Room {
         String roomDesc;
         List<String> roomFacilities;
     }
 
-  // Below function returns array which shows frequency count for searchword in various files
+    // Below function returns an array which shows frequency count for a search word in various files
     public static int[] searchSpecificWordFrequencies(String searchWord) {
         String[] fileArr = {"src/data/BookingCA.json", "src/data/HotelsCA.json", "src/data/ExpediaCA.json"};
         int[] wordFrequencyArrayHotelData = new int[fileArr.length];
         Gson gsonObj = new Gson();
-        // typeObjGson used for JSON deserialization with Google's Gson library
-        Type typeObjGson = new TypeToken<Map<String, List<HotelData>>>(){}.getType();
+        // Updated TypeToken for JSON deserialization with Google's Gson library
+        Type typeObjGson = new TypeToken<List<Map<String, List<HotelData>>>>(){}.getType();
 
         int fileIndex = 0;
-        for (String HotelDataFileName : fileArr) {
+        for (String hotelDataFileName : fileArr) {
             TreeMap<String, Integer> wordCountTM = new TreeMap<>();
 
-            try (FileReader fReader = new FileReader(HotelDataFileName)) {
-                Map<String, List<HotelData>> hotelsData = gsonObj.fromJson(fReader, typeObjGson);
-                processHotelData(hotelsData, wordCountTM);
+            try (FileReader fReader = new FileReader(hotelDataFileName)) {
+                // Deserialize JSON to the new structure
+                List<Map<String, List<HotelData>>> listOfHotelDataMaps = gsonObj.fromJson(fReader, typeObjGson);
+
+                // Iterate through the list and then through each map
+                for (Map<String, List<HotelData>> hotelsData : listOfHotelDataMaps) {
+                    processHotelData(hotelsData, wordCountTM);
+                }
+
                 wordFrequencyArrayHotelData[fileIndex++] = wordCountTM.getOrDefault(searchWord, 0);
             } catch (Exception e) {
-                System.out.println("Error processing file: " + HotelDataFileName);
+                System.out.println("Error processing file: " + hotelDataFileName);
             }
         }
         return wordFrequencyArrayHotelData;
@@ -70,19 +77,20 @@ public class FrequencyCount {
 
     private static void processText(String text, TreeMap<String, Integer> wordCountTM) {
         if (text != null) {
-        	// Split the text into words using non-word characters as delimiters
+            // Split the text into words using non-word characters as delimiters
             String[] words = text.toLowerCase().split("\\W+");
             for (String word : words) {
-            	// Check if the word is non-empty and not just a number
+                // Check if the word is non-empty and not just a number
                 if (!word.isEmpty() && !word.matches("\\d+")) {
-                	// Increment the word count in the TreeMap
+                    // Increment the word count in the TreeMap
                     wordCountTM.put(word, wordCountTM.getOrDefault(word, 0) + 1);
                 }
             }
         }
     }
+
     public static void main(String[] args) {
-        String searchWord = "breakfast"; //This is a Example keyword
+        String searchWord = "breakfast"; // This is an example keyword
         int[] frequencies = FrequencyCount.searchSpecificWordFrequencies(searchWord);
         System.out.println("Frequencies of '" + searchWord + "': " + Arrays.toString(frequencies));
     }
